02e9bb2fe790858282a89ad9250ce9a6dc2529be
Ashley Stojak <68790374+astojak@users.noreply.github.com>
2021-11-30 13:51:30 -0600

    final code

--- CS476_FinalProj.ml
+++ CS476_FinalProj.ml
@@ -1,3 +1,8 @@
+(* Authors: Ashley Stojak (653366198)
+            Shruti Italia (674095977)
+            Paola Perez-Rivera (653901920)
+*)
+
 open List
 
 type exp = Num of float
@@ -27,12 +32,12 @@ let findMax b findingMax =
 
 
 let rec findAllJ b c1 c2 i j max = 
-        if (j = max+.1.) then (0.0,0.0) else 
+        if (j >= max+.1.) then (0.0,0.0) else 
                 if (c1 *. i +. c2 *. j) = b then (i,j) else (findAllJ b c1 c2 i (j+.1.) max)
 
 
 let rec findAll max secondMax b c1 c2 i = 
-        if (i = max+.1.) then [] else 
+        if (i >= max+.1.) then [] else 
                 let j = (findAllJ b c1 c2 i 0. secondMax) in
                 if (j = (0.0,0.0)) then (findAll max secondMax b c1 c2 (i+.1.)) else 
                 j::(findAll max secondMax b c1 c2 (i+.1.))
@@ -67,5 +72,39 @@ let rec eval_exp ( e:exp ) =
                                                                                               Some(FloatTuple(findAll max secondMax f1 f2 f3 0.))
                                 | _ -> None);;
 
-let test1 = eval_exp (Budget (Num 10.0, Num 2.0, Num 0.5));;
-let test2 = eval_exp (Budget (Num 36.0, Num 4.0, Num 3.0));;
+(* tests for num *)
+(* let e = eval_exp( Num 5) (* throws an error because expects float, not int *) *)
+let e2 = eval_exp( Num 5.) (* return Some( FloatVal  5.)*)
+
+(* tests for NumList *)
+(* let el = eval_exp( NumList[1;2;3;4;5]) (* throws an error because expects float list, not int list*)*)
+let el = eval_exp( NumList[ 1.; 2.; 3.; 4.; 5.]);; (* return Some( FloatList [1.;2.;3.;4.;5.]) *)
+
+(* tests for EarnedVal *)
+let earned_test1 = eval_exp( EarnedVal( Num 5., Num 4.));; (* return Some( FloatVal 20.) *)
+
+(* CostVar tests *)
+let costvar_test1 = eval_exp( CostVar( EarnedVal( Num 5., Num 4.), ActualVal( Num 8., Num 9.)));; (* return Some( FloatVal (0.27777779)) *)
+let costvar_test2 = eval_exp( CostVar( Num 7., Num 0.)) (* return None *)
+let costvar_test3 = eval_exp( CostVar( Num 7., Num 2.)) (* return Some(FloatVal 3.5) *)
+
+(* CostPer tests *)
+let costper_test1 = eval_exp( CostPer( EarnedVal( Num 8., Num 2.), ActualVal( Num 9., Num 3.)));; (* return Some( FloatVal ( -11.)) *)
+let costper_test2 = eval_exp( CostPer( Num 7., Num 4.));; (* return Some( FloatVal 3.) *)
+
+(* tests for sum *)
+let sum_test1 = eval_exp (Sum (NumList [3.0; 6.0; 9.0; 23.0]));; (* should be (FloatVal 41.) *)
+let sum_test2 = eval_exp (Sum (NumList [5.0; 10.0; 6.1; 0.8]));; (* should be (FloatVal 21.9) *)
+let sum_test3 = eval_exp( Sum( NumList [1.; 2.; 3.; 4.; 5.]));; (* return Some( FloatVal 15.) *)
+let avg_test4 = eval_exp( Avg( NumList [1.; 2.; 3.; 4.; 5.]));; (* return Some( FloatVal 3.) *)
+
+(* tests for average *)
+let avg_test1 = eval_exp (Avg (NumList [1.0; 4.0; 5.0; 8.0]));; (* should be (FloatVal 4.5) *)
+let avg_test2 = eval_exp (Avg (NumList [8.0; 9.0; 20.1; 5.8]));; (* should be (FloatVal 10.7250000000000014) *)
+let avg_test3 = eval_exp( Avg( NumList [1.; 2.; 3.; 4.; 5.]));; (* return Some( FloatVal 3.) *)
+
+(* tests for budget *)
+let budget_test1 = eval_exp (Budget (Num 10.0, Num 2.0, Num 0.5));; (* should be Some (FloatTuple [(0., 20.); (1., 16.); (2., 12.); (3., 8.); (4., 4.); (5., 0.)]) *)
+let budget_test2 = eval_exp (Budget (Num 36.0, Num 4.0, Num 3.0));; (* should be Some (FloatTuple [(0., 12.); (3., 8.); (6., 4.); (9., 0.)]) *)
+let budget_test3 = eval_exp (Budget (Num 36.0, Num 2.0, Num 12.0));; (* should be Some (FloatTuple [(0., 3.); (6., 2.); (12., 1.); (18., 0.)]) *)
+let budget_test4 = eval_exp (Budget (Num 36.0, Num 2.0, Num 5.0));; (* should be Some (FloatTuple [(3., 6.); (8., 4.); (13., 2.); (18., 0.)]) *)
